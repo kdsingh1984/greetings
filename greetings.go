e62e2460c865a105a7314a7dcf1f2cd71e007068
Kamaldeep Singh <kamaldeep_singh@apple.com>
2022-02-09 10:53:44 -0600

    greetings: error for empty name v0.0.2

--- greetings.go
+++ greetings.go
@@ -1,7 +1,13 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello %s, welcome", name)
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	return fmt.Sprintf("Hello %s, welcome", name), nil
 }
