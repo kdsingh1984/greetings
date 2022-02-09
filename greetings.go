06c93a50c3d67f57a0428609701fa6f911b637ea
Kamaldeep Singh <kamaldeep_singh@apple.com>
2022-02-09 13:30:39 -0600

    greetings: random message v0.0.3

--- greetings.go
+++ greetings.go
@@ -3,11 +3,27 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	return fmt.Sprintf("Hello %s, welcome", name), nil
+
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	format := []string{
+		"Hello %s, welcome",
+		"Hey %s, Good to see you",
+		"Holla %s",
+	}
+	return format[rand.Intn(len(format))]
 }
