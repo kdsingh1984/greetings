6132554169e1dcf81f3f7d7180ec8066d2ad339b
Kamaldeep Singh <kamaldeep_singh@apple.com>
2022-02-09 13:49:26 -0600

    greetings: implemented Hellos v0.0.4

--- greetings.go
+++ greetings.go
@@ -15,6 +15,18 @@ func Hello(name string) (string, error) {
 	return fmt.Sprintf(randomFormat(), name), nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	msgMap := make(map[string]string)
+	for _, name := range names {
+		msg, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		msgMap[name] = msg
+	}
+	return msgMap, nil
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
